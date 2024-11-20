f9579ddf7fa0800991c69c5e6820e3bf908570e9
Noccis <toniwincent83@gmail.com>
2024-11-20 10:42:45 +0100

    API connected

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import Header from './components/Header'
 import PlayerSection from './components/PlayerSection'
 import HomePage from './pages/HomePage'
+import TriviaPage from './pages/TriviaPage'
 
 function App() {
   return (
     <div id='main-container'>
       <Header />
       <div id='quiz-container'>
-        <HomePage />
+        <TriviaPage />
+        {/* <HomePage /> */}
         <PlayerSection />
       </div>
     </div>
---
+++ src/components/styled/TriviaPage.styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledTriviaPage = styled.div`
+height: 100%;
+width: 75%;
+padding: 1%;
+`
