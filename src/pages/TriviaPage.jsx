2306c989e4ea5fabc67e8ce6b8fa7992f2976a3c
Noccis <toniwincent83@gmail.com>
2024-11-21 15:11:40 +0100

    Landing page is now showing before game starts

--- src/pages/TriviaPage.jsx
+++ src/pages/TriviaPage.jsx
@@ -1,9 +1,9 @@
 import React, { useRef, useState } from "react";
 import { StyledTriviaPage } from "../components/styled/TriviaPage.styled";
-import { useQuestions } from '../context/QuestionContext';
+import { useQuestions } from "../context/QuestionContext";
 import QuestionContainer from "../components/QuestionContainer";
 import GameOver from "../components/GameOver";
-
+import Landing from "../components/Landing";
 
 const TriviaPage = () => {
   // Values from the useContext:
@@ -11,52 +11,68 @@ const TriviaPage = () => {
   const [playerScore, setPlayerScore] = useState(0);
   const selectedAnswer = useRef("");
   const [questionIndex, setQuestionIndex] = useState(0);
+  const [startGame, setStartGame] = useState(false);
 
-  function isAnswerRight(){
-    if(selectedAnswer.current == questions[questionIndex].correct_answer){
+  // Controll if the guess is correct and will generate a point
+  function isAnswerRight() {
+    if (selectedAnswer.current == questions[questionIndex].correct_answer) {
       console.log("Du gissade rätt!!");
-      setPlayerScore(playerScore + 1);   
-    }else{
+      setPlayerScore(playerScore + 1);
+    } else {
       console.log("FEEEEEEEEL!");
     }
     setQuestionIndex(questionIndex + 1);
   }
-  
+
+  // Take the right answer and put it somewhere random in the array of answers
   function getArrayWithChoices() {
-    if(questions.length > 0){
-      if(Array.isArray(questions[questionIndex].incorrect_answers)){
-        let arrayOfChoices = [...questions[questionIndex].incorrect_answers, questions[questionIndex].correct_answer]
-        return arrayOfChoices
-      }else{
-        let arrayOfChoices = [questions[questionIndex].incorrect_answers, questions[questionIndex].correct_answer]
-        return arrayOfChoices
+    if (questions.length > 0) {
+      if (Array.isArray(questions[questionIndex].incorrect_answers)) {
+        let arrayOfChoices = [
+          ...questions[questionIndex].incorrect_answers,
+          questions[questionIndex].correct_answer,
+        ];
+        return arrayOfChoices;
+      } else {
+        let arrayOfChoices = [
+          questions[questionIndex].incorrect_answers,
+          questions[questionIndex].correct_answer,
+        ];
+        return arrayOfChoices;
       }
-      
     }
   }
 
   return (
     <StyledTriviaPage>
-      <button onClick={getQuestions} disabled={loading}>
-        Fetch Questions
-      </button>
+      {!startGame && (
+        <>
+          <Landing />
+          <button onClick={()=> {
+            setStartGame(true);
+            getQuestions();
+          }} disabled={loading}>
+            Starta spelet
+          </button>
+        </>
+      )}
+
       {loading && <p>Loading...</p>}
       {error && <p>{error}</p>}
-      {questions.length > 0 && (
-       questionIndex < questions.length ? (
-        <>
-          <QuestionContainer 
-            question={atob(questions[questionIndex].question)}
-            choices={getArrayWithChoices()}
-            selectedAnswer={selectedAnswer}
-            index={questionIndex + 1}
-          />
-          <button onClick={isAnswerRight}>Next</button>
-        </>
-      ) : (
-        <GameOver playerScore={playerScore} />
-      )
-      )}
+      {questions.length > 0 &&
+        (questionIndex < questions.length ? (
+          <>
+            <QuestionContainer
+              question={atob(questions[questionIndex].question)}
+              choices={getArrayWithChoices()}
+              selectedAnswer={selectedAnswer}
+              index={questionIndex + 1}
+            />
+            <button onClick={isAnswerRight}>Next</button>
+          </>
+        ) : (
+          <GameOver playerScore={playerScore} />
+        ))}
     </StyledTriviaPage>
   );
 };
