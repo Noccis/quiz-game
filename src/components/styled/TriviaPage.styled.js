ddb22552f0927ed6987c4f7406c8be085c1a8137
Noccis <toniwincent83@gmail.com>
2024-11-23 15:15:34 +0100

    small fix on the styling of buttons

--- src/components/styled/TriviaPage.styled.js
+++ src/components/styled/TriviaPage.styled.js
@@ -9,13 +9,16 @@ export const StyledTriviaPage = styled.div`
   align-items: center;
 
   button {
-    width: 200px;
-    height: 100px;
     margin: 20px;
     padding: 5px;
     background-color: rgb(12, 88, 159);
     color: white;
     border-radius: 7px;
+  }
+
+  #large-button {
+    width: 200px;
+    height: 100px;
     font-size: 1.5rem;
   }
-`;
+`
