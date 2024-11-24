6415d8b6db272c630fb7f06f8b7890be2ecc442e
Noccis <toniwincent83@gmail.com>
2024-11-24 20:14:16 +0100

    Added useMemo

--- src/components/QuestionContainer.jsx
+++ src/components/QuestionContainer.jsx
@@ -1,32 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
 import { StyledQuestionContainer } from "./styled/QuestionContainer.styled";
 
 const QuestionContainer = ({ question, choices, selectedAnswer, index }) => {
-  
-
   const handleAnswerSelect = (answer) => {
     console.log("value: " + atob(answer));
     selectedAnswer.current = answer;
     console.log("Selected answer: " + atob(selectedAnswer.current));
   };
 
-  useEffect(()=> {
+  useEffect(() => {
     const rightAnswer = choices.length - 1;
     const value = atob(choices[rightAnswer]);
-    console.log("Rätt svar: " + value);  
-  },[])
+    console.log("Rätt svar: " + value);
+  }, [choices]);
 
-  const moveLastToRandomIndex = (array) => {
-    if (array.length < 2) return array; 
-  
-    const lastElement = array.pop(); // Remove the last element.
-    const randomIndex = Math.floor(Math.random() * array.length); // Generate a random index.
-  
-    array.splice(randomIndex, 0, lastElement); // Insert the last element at the random index.
-    return array;
-  };
+  // I use useMemo to shuffle the answers only when `choices` changes because I want the order of the answers to remain until next question
+  const shuffledArrayOfChoises = useMemo(() => {
+    const moveLastToRandomIndex = (array) => {
+      if (array.length < 2) {
+        return array;
+      }
 
-  let shuffledArrayOfChoises = moveLastToRandomIndex(choices);
+      const lastElement = array.pop();
+      const randomIndex = Math.floor(Math.random() * array.length);
+      array.splice(randomIndex, 0, lastElement);
+      return array;
+    };
+
+    return moveLastToRandomIndex([...choices]); // Create a copy of the choices to avoid mutating the original array
+  }, [choices]); // Only recompute when `choices` changes
 
   return (
     <StyledQuestionContainer>
